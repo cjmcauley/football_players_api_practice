2e23290a40b6a21fad390d73797f69f0f122ef3b
cjmcauley <christopher.mcauley@gmail.com>
2019-08-09 13:15:16 +0100

    fetch from API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+<h1>Players</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      players: []
+    };
+  },
+  mounted(){
+    fetch('https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=Arsenal')
+    .then(result => result.json())
+    .then(players => this.players = players)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
