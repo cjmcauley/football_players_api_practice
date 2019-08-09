c86f9985e2a67b900fda93cb2415df4207d89917
cjmcauley <christopher.mcauley@gmail.com>
2019-08-09 15:47:45 +0100

    table added to display player information

--- src/components/PlayerInfo.vue
+++ src/components/PlayerInfo.vue
@@ -1,9 +1,22 @@
 <template lang="html">
+<div>
 
-<ul>
-  <li>{{ player.strPlayer }}</li>
-</ul>
+<table v-if="player">
+  <tr>
+    <td colspan="2">{{ player.strPlayer }}</td>
+    <td>{{ player.strNumber }}</td>
+  </tr>
+  <tr>
+    <td>Date of Birth:</td>
+    <td>{{ player.dateBorn }}</td>
+  </tr>
+  <tr>
+    <td>Nationality:</td>
+    <td>{{ player.strNationality }}</td>
+  </tr>
+</table>
 
+</div>
 </template>
 
 <script>
@@ -14,4 +27,8 @@ export default {
 </script>
 
 <style lang="css" scoped>
+td, th {
+  border: 1px solid #dddddd;
+  width: 200px;
+}
 </style>
