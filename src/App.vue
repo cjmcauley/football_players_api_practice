7fed3e8faf10646ed426e288f5f2e4477f6eaa2a
cjmcauley <christopher.mcauley@gmail.com>
2019-08-09 14:18:14 +0100

    dropdown populated

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template lang="html">
-<h1>Players</h1>
+<div >
+  <h1>Players</h1>
+    <player-select :players="players" />
+  </div>
 </template>
 
 <script>
+import { eventBus } from './main.js';
+import PlayerSelect from './components/PlayerSelect.vue'
+
 export default {
   name: 'app',
+  components: {
+    'player-select': PlayerSelect
+  },
   data() {
     return {
       players: []
@@ -13,6 +22,7 @@ export default {
   mounted(){
     fetch('https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=Arsenal')
     .then(result => result.json())
+    .then(players => players = players.player)
     .then(players => this.players = players)
   }
 }
---
+++ src/components/PlayerSelect.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <select>
+    <option disabled option selected='selected'>Select Player</option>
+    <option v-for="(player, index) in players">{{ player.strPlayer }}</option>
+  </select>
+</template>
+
+<script>
+import { eventBus } from '../main.js';
+
+export default {
+  name: 'player-select',
+  props: ['players']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
